8207183d3118d297f04b86fe9ba03e7c382881f7
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2020-03-31 11:29:47 +0600

    Add console to tract the number

--- magic.js
+++ magic.js
@@ -3,6 +3,7 @@
 // 3. write "var emails = " and pase your formated email that you will get from Sumon
 // the code will be like var emails = ['email1', 'email2']
 // 4. Copy and Past the below code on your console and press enter button
+// 
 
 var i = 0;
 var sleep_time = 40000; // in milisecond that means 40 seconds
@@ -10,7 +11,7 @@ var sleep_time = 40000; // in milisecond that means 40 seconds
     $('.btn.btn-orange.m-1[data-company="65586"]').click();
     $('#vote-form-email').val(emails[i]);
     $('.btn.btn-orange[type="submit"').click();
-
+    console.log('current email number is ' + i);
     if (++i < emails.length) {
         setTimeout(loop, sleep_time);
     }
